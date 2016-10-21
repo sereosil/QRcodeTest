c076bdf5c52da378dd6924ae81ec346a7b77443d
sereosil <sereosil@gmail.com>
2016-10-21 16:43:25 +0300

    все что нужно

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -10,9 +10,11 @@ import com.lowagie.text.*;
 import com.lowagie.text.Image;
 import com.lowagie.text.pdf.*;
 import javafx.scene.Node;
+import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.printing.PDFPageable;
+import org.omg.IOP.ENCODING_CDR_ENCAPS;
 
 import javax.imageio.ImageIO;
 import javax.print.*;
@@ -22,6 +24,7 @@ import javax.print.attribute.PrintServiceAttribute;
 import javax.print.attribute.PrintServiceAttributeSet;
 import javax.print.event.PrintServiceAttributeListener;
 import java.awt.*;
+import java.awt.Font;
 import java.awt.image.BufferedImage;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
@@ -29,6 +32,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.UUID;
@@ -60,11 +64,29 @@ public class Testing {
         com.lowagie.text.Image qrCode = Image.getInstance("C:/Users/sereo_000/Downloads/123.png");
         com.lowagie.text.Image watermark = Image.getInstance("C:/Users/sereo_000/Downloads/111.png");
         String pathWhereQrCodeWillBeSaved = tempFilePath+"/"+filenameOfQR+".png";
-        String attributeOfFile = "1234567890qwertyuiopasdfghjklzxcvbnm";
+        String collectionNumber = "123";
+        Date currentDate;
+        currentDate = new Date(System.currentTimeMillis());
+        String koi8Data = new String();
+
+        String attributeOfFile = new String("Номер серии: "+collectionNumber+" Дата и время печати: "+currentDate+" Напечатал: ");
+        System.out.println(attributeOfFile);
+        int width = 650;
+        int height = 30;
+        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = bi.createGraphics();
+        g2.setColor(Color.WHITE);
+        g2.fillRect(0,0, width, height);
+        g2.setStroke(new BasicStroke(20.0f));
+        g2.setColor(Color.black);
+        g2.setFont(new Font("Arial",1,10));
+        g2.drawString(attributeOfFile,10,15);
+        ImageIO.write(bi, "jpg",new File("C:/Users/sereo_000/Downloads/qq.jpg"));
+        com.lowagie.text.Image qeqe = Image.getInstance("C:/Users/sereo_000/Downloads/qq.jpg");
         int sizeOfQrCode = 250;
         String fileType = "png";
         QrCodeMaking(textInQrCode,fileType,sizeOfQrCode,pathWhereQrCodeWillBeSaved);
-        WatermarkAndQrCodeAddingOnFile(pdfFileForPrint,pdfFileWithWatermarkAndQrCode,qrCode,watermark,attributeOfFile);
+        WatermarkAndQrCodeAddingOnFile(pdfFileForPrint,pdfFileWithWatermarkAndQrCode,qrCode,watermark,attributeOfFile,qeqe);
         PrintingFile(pdfFileWithWatermarkAndQrCode,args);
         pdfFileWithWatermarkAndQrCode.deleteOnExit();
     }
@@ -107,7 +129,7 @@ public class Testing {
         }
         System.out.println("\n\nYou have successfully created QR Code.");
     }
-    private static void WatermarkAndQrCodeAddingOnFile(File pdfFileForPrint,File pdfFileWithWatermarkAndQrCode, Image qrCode,Image watermark, String attributeOfFile) throws IOException, DocumentException {
+    private static void WatermarkAndQrCodeAddingOnFile(File pdfFileForPrint, File pdfFileWithWatermarkAndQrCode, Image qrCode, Image watermark, String attributeOfFile, Image g2) throws IOException, DocumentException {
         watermark.setAlignment(Element.ALIGN_CENTER);
         boolean a = pdfFileForPrint.canWrite();
         PdfReader reader = new PdfReader(pdfFileForPrint.getPath());
@@ -119,6 +141,8 @@ public class Testing {
             PdfContentByte content2 = stamper.getOverContent(i);
             qrCode.setAbsolutePosition(530f, 780f);
             qrCode.scaleAbsolute(50f,50f);
+            g2.setAbsolutePosition(0f,800f);
+            g2.scaleAbsolute(500f,30f);
             watermark.setAbsolutePosition(140f, 350f);
             watermark.setRotationDegrees(45);
             content.addImage(qrCode);
@@ -127,7 +151,10 @@ public class Testing {
 // this are the coordinates where you want to add text
 // if the text does not fit inside it will be cropped
             ct.setSimpleColumn(30,835,750,50);
-            ct.setText(new Phrase(attributeOfFile));
+            String attributeTest = new String(attributeOfFile.getBytes("ISO-8859-5"),"utf8");
+            //content2.addImage(g2);
+            content2.addImage(g2);
+            //ct.setText(new Phrase(String.valueOf(g2)));
             ct.go();
         }
         stamper.getWriter().addDirectImageSimple(qrCode);
